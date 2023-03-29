d464a09f9eed584b97360b5f954081f85d161f06
Samuel Becerra <samuelb0279@gmail.com>
2023-03-28 22:48:52 -0600

    Tercer version

--- app.js
+++ app.js
@@ -8,40 +8,8 @@ let connection = mysql.createConnection({
 });
 connection.connect();
 
-/*connection.query(
+connection.query(
     `
-    CREATE TABLE Clientes(
-        id int AUTO_INCREMENT, 
-        Nombre varchar(50), 
-        RFC varchar(50), 
-        Ciudad varchar(20), 
-        CP char(20), 
-        Email varchar(50), 
-        PRIMARY KEY(id));
-
-    CREATE TABLE Productos(
-        id int AUTO_INCREMENT, 
-        Nombre varchar(50), 
-        Costo char(20), 
-        Cantidad char(100), 
-        PRIMARY KEY(id));
-
-    CREATE TABLE Facturas(
-        Folio int AUTO_INCREMENT, 
-        Fecha DATETIME, 
-        Total char(50), 
-        ClienteID int not null, 
-        PRIMARY KEY(Folio), 
-        FOREIGN KEY(ClienteID) REFERENCES clientes(id));
-
-    CREATE TABLE Detalle_Facturas(
-        id int AUTO_INCREMENT, 
-        Factura int UNIQUE, 
-        Producto int UNIQUE, 
-        Cantidad int, 
-        Costo int, 
-        PRIMARY KEY(id));
-
     INSERT INTO Productos (Nombre, Costo, Cantidad) 
         VALUES('Lápiz', CONCAT('$', FORMAT(10, 2)), 100);
 
@@ -57,25 +25,49 @@ connection.connect();
         console.log(results);
         console.log(fields);
     }
-});*/
+});
 
 let cliente = {
     Nombre:'Nepomuceno',
     RFC:'NEPO231010',
     Ciudad:'Colima',
     CP: 28018,
-    Email:'CorreoNEP'
+    Email:'Nepo@gmail.com'
 };
+let cli_name = cliente.Nombre;
+let cli_RFC = cliente.RFC;
+let cli_Ciudad = cliente.Ciudad;
+let cli_CP = cliente.CP;
+let cli_Email = cliente.Email;
 
-let fac_name = cliente.Nombre;
-let fac_RFC = cliente.RFC;
-let fac_Ciudad = cliente.Ciudad;
-let fac_CP = cliente.CP;
-let fac_Email = cliente.Email;
+let factura = {
+    fecha:'2023/03/23',
+    total: "",
+    productos: [
+        {
+            id: 1,
+            cantidad: 5,
+            costo: 10
+        },
+        {
+            id: 2,
+            cantidad: 5,
+            costo: 20
+        }
+    ]
+};
+factura.total = factura.productos[0].cantidad*factura.productos[0].costo + factura.productos[1].cantidad*factura.productos[1].costo;
+
+let fac_fecha = factura.fecha;
+let fac_total = factura.total;
+let fac_ID_product1 = factura.productos[0].id;
+// let fac_cantidad_product1 = fac_fecha.productos[0].cantidad;
+// let fac_costo_product1 = fac_fecha.productos[0].costo;
+
 
 connection.query(`
-INSERT INTO Clientes (Nombre, RFC, Ciudad, CP) 
-    VALUES (${fac_name}, ${fac_RFC}, ${fac_Ciudad}, ${fac_CP}, ${fac_Email});
+INSERT INTO Clientes (Nombre, RFC, Ciudad, CP, Email) 
+    VALUES ("${cli_name}", "${cli_RFC}", "${cli_Ciudad}", "${cli_CP}", "${cli_Email}");
 `, function(error, results, fields){
         if (error){
             throw error;
@@ -85,5 +77,29 @@ INSERT INTO Clientes (Nombre, RFC, Ciudad, CP)
         }
     }
 );
-
-connection.end();
+try{
+await connection.query(`
+    SELECT id FROM clientes WHERE Nombre = "${cli_name}"
+`, function(error, results, fields){
+    if(error){
+        throw error;
+    } else{
+        let id_cliente = console.log(results[0].id);
+        connection.query(`
+        INSERT INTO facturas (Folio, Fecha, Total, ClienteID) 
+            VALUES (1010, "${fac_fecha}", "${fac_total}", ${id_cliente})
+    `, function(error, results, fields){
+        if(error){
+            throw error;
+        } else{
+            console.log(results);
+            console.log(fields);
+        }
+    });
+    }
+});
+} catch(error){
+    console.log(error);
+} finally{
+    connection.end();
+}
